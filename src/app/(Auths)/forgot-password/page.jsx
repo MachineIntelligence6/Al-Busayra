c960408e82b7d97d77839025d1e898587db1d393
Bilal khan <muhammadbilal@mi6.global>
2024-12-12 13:31:54 +0500

    forgot password bug fixed

--- src/app/(Auths)/forgot-password/page.jsx
+++ src/app/(Auths)/forgot-password/page.jsx
@@ -1,7 +1,7 @@
+"use client"
 import React from "react";
 import Login from "@/components/Auth-components/Login";
 import ForgotPassword from "@/components/Auth-components/ForgotPassword";
-import AuthLayout from "@/Layouts/AuthLayout";
 
 const page = () => {
   return (
