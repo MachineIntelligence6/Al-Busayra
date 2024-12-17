4bd8b4e182d20413b6ba0d6e6630ee71d2a151fd
ahsan798 <itsmeahsan98@gmail.com>
2024-12-17 20:15:26 +0500

    resolved conflicts

--- src/components/company-flow/employees/advance-salary/index.jsx
+++ src/components/company-flow/employees/advance-salary/index.jsx
@@ -5,15 +5,9 @@ import { Plus } from "lucide-react";
 import React, { useState } from "react";
 import EmptyScreenView from "@/components/Shared-components/EmptyScreenView";
 import CompanyEmpty from "../../CompanyEmpty";
-import CustomBreadcrumb from "@/app/Components/sharedComponents/BreadCrum/page";
-import { Button } from "@/components/ui/button";
 import AdvanceSalaryTableModal from "./table/AdvanceSalaryTableModal";
 import AdvanceSalaryTable from "./table";
 import DynamicBreadcrumb from "@/components/Shared-components/BreadCrumb";
-<<<<<<< HEAD
-=======
-import CustomButton from "@/components/Shared-components/CustomButton";
->>>>>>> develop_next
 
 const AdvanceSalaryEmptyScreen = () => {
   const [showPopup, setShowPopup] = useState(false);
@@ -32,24 +26,9 @@ const AdvanceSalaryEmptyScreen = () => {
           alignItems: "center",
         }}
       >
-<<<<<<< HEAD
-        {/* <CustomButton startIcon={<Plus />}>Request Advance Salary</CustomButton>  */}
-        {/* <CustomBreadcrumb name="Advance Salary" /> */}
-        <DynamicBreadcrumb />
-        <Button
-=======
         <DynamicBreadcrumb />
         <CustomButton startIcon={<Plus />}>Request Advance Salary</CustomButton>
-        {/* <CustomBreadcrumb name="Advance Salary" /> */}
-
-        {/* <Button
->>>>>>> develop_next
-          className="bg-[#296291] hover:bg-[#4080b4]"
-          onClick={() => setShowPopup(true)}
-        >
-          Request Advance Salary
-          <Plus />
-        </Button> */}
+    
       </Box>
 
       <Divider sx={{ mt: 1, mx: 2, mb: 2 }} />
