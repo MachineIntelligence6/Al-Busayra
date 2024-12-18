abb1d8b73b1af943c84376c1281be68fbc316ba3
Aqeel haidar <haideraqeel86@hotmail.com>
2024-12-18 15:53:24 +0500

    add more clear

--- src/components/company-flow/general-deduction-components/index.jsx
+++ src/components/company-flow/general-deduction-components/index.jsx
@@ -2,22 +2,30 @@
 import React, { useState } from 'react'
 import CompanyEmpty from '../CompanyEmpty';
 import EmptyScreenView from '@/components/Shared-components/EmptyScreenView';
+import { Box } from '@mui/material';
+import CompanyHeader from '@/components/Shared-components/CompanyHeader';
 
 const GereralDeduction = () => {
     const [modalOpen, setModalOpen] = useState(false);
-    const [showTable, setShowTable] = useState(false);
+    const [showTable, setShowTable] = useState(true);
 
 
     return (
         <>
-            {showTable ? null : <CompanyEmpty heading="General Deduction" >
+            {showTable ? <Box>
+                <CompanyHeader heading="General Deduction" />
+                <Box sx={{ p: 2 }}>
+                    <h1>hello</h1>
+                </Box>
+            </Box> : <CompanyEmpty heading="General Deduction" >
                 <EmptyScreenView
                     image="/company/icons/general-deduction.svg"
                     buttonText="General Deduction"
                     icon={null}
                     onButtonClick={() => setModalOpen(true)}
                 />
-            </CompanyEmpty >}
+            </CompanyEmpty >
+            }
         </>
     )
 }
